22968d0b71a6c31bc4fa12970749a61d55b3561a
cmomodo <ceesay.ml@outlook.com>
2025-06-03 23:49:48 +0100

    We are now starting to learn about bash development

---
+++ bash/hello.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#this is the first part of the demo happy learning!
+name="ceesay"
+echo "My name is Momodou, $name"
+
+read -p "Enter your name:" name1
+
+echo "My name is $name1"
---
+++ bash/loop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+name="Bob"
+read -p "Enter your name: " name
+if [ $name == "Alice" ]; then echo "Hi Alice!"; fi
+
+for i in 1 2 3; do echo "Number $i"; done
